c0402684f79951048126c8130360da2d1372e17a
Guilherme Nogima <gbnogima@gmail.com>
2017-09-19 15:52:39 -0400

    Backend

--- oi.py
+++ oi.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
 import mongoengine as me 
+from datetime import datetime
 
 class User(me.Document):
 	name = me.StringField()
 	email = me.StringField()
 
+	def to_dict(self):
+		return {
+			'id': str(user._id),
+			'name': user.name,
+			'email': user.email
+			}
 
 class Task(me.Document):
 	title = me.StringField()
@@ -16,6 +23,17 @@ class Task(me.Document):
 	user = me.ReferenceField(User)
 	color = me.StringField()
 
+	def to_dict(self):
+		return {
+			'id': str(self.user.id),
+			'title': self.title,
+			'description': self.description,
+			'deadline': int(self.deadline.timestamp()),
+			'finished': self.finished,
+			'added': int(self.added.timestamp()),
+			'user': str(self.user),
+			'color': self.color,
+		}
 
 app = Flask(__name__)
 me.connect('todo_app')
@@ -25,12 +43,49 @@ def get_users():
 	users = User.objects.all()
 	array = []
 	for user in users:
-		array.append({
-			'id': str(user._id),
-			'name': user.name,
-			'email': user.email
-			})
+		array.append(user.to_dict())
 	return jsonify(array)
 
+@app.route("/users", methods = ['POST'])
+def create_user():
+	if not request.is_json:
+		return jsonify({'error': 'not json'}), 400
+	data = request.get_json()
+	name = data.get('name')
+	email = data.get('email')
+	user = User(name=name, email=email) 
+	user.save()
+	return jsonify(user.to_dict())
+
+@app.route("/tasks", methods = ['POST'])
+def create_task():
+	if not request.is_json:
+		return jsonify({'error': 'not json'}), 400
+	data = request.get_json()
+	task = Task(complete=False, added=datetime.now())
+	task.title = data.get('title')
+	task.description = data.get('description')
+	task.deadline = datetime.fromtimestamp(data.get('deadline', 0))
+	task.color = data.get('color')
+	task.tags = data.get('tags', [])
+	if 'user' in data.keys():
+		task.user = User.objects.filter(id=data.get('user')).first()
+	task.save()
+	return jsonify(user.to_dict())
+
+@app.route("/tasks/<string:task_id>", methods = ['PATCH'])
+def update_task(task_id):
+	if not request.is_json:
+		return jsonify({'error': 'not json'}), 400
+	task = Task.objects.filter(id=task_id)
+	if not task:
+		return jsonify({'error': 'not found'}), 404
+	data = request.get_json()
+	task.finished = data.get('finished', task.finished)
+	task.save()
+	return jsonify(user.to_dict())
+
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
+
+
